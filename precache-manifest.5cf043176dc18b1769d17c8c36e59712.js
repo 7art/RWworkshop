8d033834238a0b4dc8f4e549cc736bf2f4bded8f
7art <sem85cv@yandex.ru>
2020-05-08 13:39:45 +0300

    Updates

---
+++ precache-manifest.5cf043176dc18b1769d17c8c36e59712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73c6fa6e3078c84e8981b740fff942d",
+    "url": "/RWworkshop/index.html"
+  },
+  {
+    "revision": "15049455fbd0f559d387",
+    "url": "/RWworkshop/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "58eb3665fa285bc8a57b",
+    "url": "/RWworkshop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "15049455fbd0f559d387",
+    "url": "/RWworkshop/static/js/2.cb0c5377.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RWworkshop/static/js/2.cb0c5377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58eb3665fa285bc8a57b",
+    "url": "/RWworkshop/static/js/main.266f20e0.chunk.js"
+  },
+  {
+    "revision": "be0b495fdec105f9432b",
+    "url": "/RWworkshop/static/js/runtime-main.a1ddc24d.js"
+  }
+]);
